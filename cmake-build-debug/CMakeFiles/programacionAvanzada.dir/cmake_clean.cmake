e6f708a7ac9fcd04f779882bf7546311bf12a347
jc <jacosta291@gmail.com>
2021-08-22 18:08:01 -0600

    Add IDE files

---
+++ cmake-build-debug/CMakeFiles/programacionAvanzada.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programacionAvanzada.dir/main.c.o"
+  "programacionAvanzada"
+  "programacionAvanzada.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/programacionAvanzada.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
