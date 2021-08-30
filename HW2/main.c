0ba9d209ed0a8edbd6393248a88d180fe79b3eeb
Alex Helo <thealexxd25@hotmail.com>
2021-08-30 07:39:57 -0500

    vector creation

--- HW2/main.c
+++ HW2/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+ struct tVector {
+    void **items;
+    int capacity;
+    int total;
+} ;
+
 
 int main() {
     printf("Hello, World!\n");
     return 0;
+}
+
+
+void vector(){
+
+struct tVector v;
+
+struct v *capacity = 0;
+struct v *total = 0;
+struct v *items = malloc(sizeof(void *) * v.capacity);
+
+
+
 }
