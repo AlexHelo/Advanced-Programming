d27dc3260fb8cf6fddd8b72e81f688f0c042d523
Alex Helo <thealexxd25@hotmail.com>
2021-08-30 07:45:54 -0500

    Redid functions and added second vector creation

--- HW2/main.c
+++ HW2/main.c
@@ -14,14 +14,23 @@ int main() {
 }
 
 
-void vector(){
+struct tVector vector(){
+    struct tVector v;
 
-struct tVector v;
+    struct v *capacity = 0;
+    struct v *total = 0;
+    struct v *items = malloc(sizeof(void *) * v.capacity);
 
-struct v *capacity = 0;
-struct v *total = 0;
-struct v *items = malloc(sizeof(void *) * v.capacity);
-
-
-
+    return v;
 }
+
+struct tVector vectorSize(size_t n){
+    struct tVector v;
+
+    struct v *capacity = n;
+    struct v *total = 0;
+    struct v *items = malloc(sizeof(void *) * v.capacity);
+
+    return v;
+}
+
