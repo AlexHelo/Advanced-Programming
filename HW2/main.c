386db8134689a5b9ad2f651774e62417fa0bcade
jc <jacosta291@gmail.com>
2021-09-01 11:07:20 -0600

    Add linkedList implementation, add vector(), vectorSize() and vectorCopy() methods.

--- HW2/main.c
+++ HW2/main.c
@@ -2,37 +2,70 @@
 #include <stdlib.h>
 
 #define N 10;
-typedef struct  {
-    void **items;
-    int capacity;
-    int total;
-} tVector;
 
+struct Node {
+    struct Node * next;
+    void * content;
+};
+
+
+struct Node * vector() {
+    struct Node * node;
+    node->next = (struct Node *) malloc(sizeof (struct Node));
+    node->content = malloc(sizeof(void *));
+    return node;
+}
+
+
+struct Node * vectorSize(size_t n) {
+    struct Node * node;
+    struct Node * temp = node;
+    for(int i = 0; i < n; i++){
+        struct Node * newNode = (struct Node *) malloc(sizeof (struct Node));
+        newNode->content = malloc(sizeof (void *));
+        temp->next = newNode;
+        temp = newNode;
+    }
+
+    return node;
+}
+
+struct Node * vectorCopy(size_t n, void * content) {
+    struct Node * node = (struct Node *) malloc(sizeof (struct Node));
+    node->content = malloc(sizeof(void *));
+    node->content = content;
+    struct Node * temp = &node;
+
+    for(int i = 0; i < n; i++){
+        struct Node * newNode  = (struct  Node *) malloc(sizeof (struct Node));
+        newNode->next = NULL;
+        newNode->content = malloc(sizeof (content));
+        newNode->content = content;
+        temp->next = newNode;
+        temp = newNode;
+    }
+
+    return node;
+}
+
+void printNodes(struct Node * node) {
+
+    while(node) {
+        printf(node->content,"\n");
+        node = node->next;
+    }
+
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int integer = 5;
+    size_t n = 2;
+    struct Node * test = vectorCopy(n, (void *) integer);
+    printNodes(test);
+
     return 0;
 }
 
 
-tVector vector() {
 
-    tVector v;
-
-    v.capacity = N;
-    v.total = 0;
-    v.items = malloc(v.capacity * sizeof(void *));
-
-    return v;
-}
-
-tVector vectorSize(size_t n){
-    tVector v;
-
-    v.capacity = n;
-    v.total = 0;
-    v.items = malloc(v.capacity * sizeof(void *));
-
-    return v;
-}
 
