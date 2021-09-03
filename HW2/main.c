16d1b36a37694306cc759356f3cf07b3128ef675
jc <jacosta291@gmail.com>
2021-09-02 18:14:56 -0600

    Add insertN() and countNodes() functions

--- HW2/main.c
+++ HW2/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define N 10;
 
@@ -35,7 +36,7 @@ struct Node * vectorCopy(size_t n, void * content) {
     node->content = content;
     struct Node * temp = &node;
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++) {
         struct Node * newNode  = (struct  Node *) malloc(sizeof (struct Node));
         newNode->next = NULL;
         newNode->content = malloc(sizeof (content));
@@ -60,13 +61,73 @@ void insert(struct Node * node, int pos, void * content) {
     newNode->next = aux;
     temp->next = newNode;
 }
-void insertN(int pos, size_t n, void * content) {
 
+struct Node * insertN(struct Node * node, int pos, size_t n, void * content) {
+
+    struct Node * temp = node;
+    if(pos == 0) {
+        struct Node * newNode = (struct Node *) malloc(sizeof (struct Node *));
+        newNode->content = malloc(sizeof (void *));
+        newNode->content = content;
+        node = newNode;
+        struct Node * aux = node;
+        for(int i = 0; i < n-1; i++) {
+            struct Node * newNode1 = (struct Node *) malloc(sizeof (struct Node *));
+            newNode1->content = malloc(sizeof (void *));
+            newNode1->content = content;
+            aux->next = newNode1;
+            aux = aux->next;
+        }
+        aux->next = temp;
+        return newNode;
+    }
+
+    struct Node * previous;
+    for(int i = 0; i < pos; i++) {
+        previous = temp;
+        temp = temp->next;
+    }
+
+    for(int j = 0; j < n; j++) {
+        struct Node * newNode = (struct Node *) malloc(sizeof (struct Node *));
+        newNode->content = malloc(sizeof (void *));
+        newNode->content = content;
+        previous->next = newNode;
+        previous = previous->next;
+    }
+
+    previous->next = temp;
+    return node;
+}
+
+void pushBack(struct Node * node, void * content) {
+    struct Node * temp = node;
+    while(temp->next) {
+        temp = temp->next;
+    }
+    struct Node * newNode = (struct Node *) malloc(sizeof(struct Node));
+    newNode->content = malloc(sizeof(void *));
+    newNode->content = content;
+    temp->next = newNode;
+}
+
+void popBack(struct Node * node) {
+    struct Node * temp = node;
+    struct Node * previous;
+
+    while(temp->next){
+        previous = temp;
+        temp = temp->next;
+    }
+    previous->next = NULL;
+    free(previous->next);
+    free(temp);
 }
 
 void printNodes(struct Node * node) {
 
     while(node) {
+        printf("\n");
         printf(node->content);
         node = node->next;
     }
@@ -82,15 +143,37 @@ void freeNodes(struct Node * node) {
     }
 }
 
+bool empty(struct Node * node) {
+    return !node;
+}
+
+int countNodes(struct Node * node) {
+    struct Node * temp = node;
+    int count = 0;
+    while (temp) {
+        temp = temp->next;
+        count++;
+    }
+    return count;
+}
+
 int main() {
     int integer = 5;
     char * string = "hola";
+    char * other = "aqui";
     size_t n = 2;
     struct Node * test = vector();
+    if (empty(test)){
+        printf("Empty list");
+    }
+    printf("%d\n", countNodes(test));
+    pushBack(test, string);
+    printf("%d\n", countNodes(test));
+    test = insertN(test, 0, 3, other);
+    printf("%d\n", countNodes(test));
 
-    insert(test, 0, (void *) string);
-    insert(test, 1, (void *) string);
     printNodes(test);
+
     freeNodes(test);
 
     return 0;
